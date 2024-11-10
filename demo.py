ffed3c1e545be83c32c2440ff28f70081dbf9935
azhamoidzin <azhamoidzin@gmail.com>
2024-11-10 21:44:49 +0300

    registration demo

--- demo.py
+++ demo.py
@@ -1,14 +1,17 @@
+import smtplib
 from datetime import datetime, timedelta, timezone
+from email.mime.text import MIMEText
 from typing import Annotated
 import io
 import jwt
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
-from jwt.exceptions import InvalidTokenError
+from jwt.exceptions import InvalidTokenError, PyJWTError
 from passlib.context import CryptContext
-from pydantic import BaseModel, PositiveInt, AfterValidator
+from pydantic import BaseModel, PositiveInt, AfterValidator, EmailStr
+from config import config
 
 # to get a string like this run:
 # openssl rand -hex 32
@@ -17,13 +20,28 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
+
 fake_users_db = {
     "johndoe": {
+        'user_id': 0,
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
         "hashed_password": "$2a$10$KSEpyXKj/a0KuV/z8eutQOpE9J6juwowmJO83fUzUp.u3oFdFP8GK",
         "disabled": False,
+        "family_id": 0,
+        'registration_date': '1970-01-01',
+
+    },
+    'vasia': {
+        'user_id': 1,
+        "username": "vasia",
+        "full_name": "Vasia Piatkin",
+        "email": "vasia@example.com",
+        "hashed_password": "$2a$10$KSEpyXKj/a0KuV/z8eutQOpE9J6juwowmJO83fUzUp.u3oFdFP8GK",
+        "disabled": False,
+        "family_id": 0,
+        'registration_date': '2024-01-01',
     }
 }
 
@@ -63,6 +81,7 @@ class User(BaseModel):
     email: str | None = None
     full_name: str | None = None
     disabled: bool | None = None
+    family_id: int | None = None
 
 
 class UserInDB(User):
@@ -98,7 +117,7 @@ class FeederUpdate(BaseModel):
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 app = FastAPI()
 origins = ["*"]
@@ -128,7 +147,7 @@ def get_user(db, username: str):
 
 def authenticate_user(fake_db, username: str, password: str):
     user = get_user(fake_db, username)
-    if not user:
+    if not user or user.disabled:
         return False
     if not verify_password(password, user.hashed_password):
         return False
@@ -174,7 +193,7 @@ async def get_current_active_user(
     return current_user
 
 
-@app.post("/token")
+@app.post("/login")
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ) -> Token:
@@ -266,3 +285,103 @@ async def read_own_feeders(
     new_feeder = {**fake_feeders_db[current_user.username][feeder_id], **feeder_update}
     fake_feeders_db[current_user.username][feeder_id] = new_feeder
     return new_feeder
+
+
+fake_families_db = {
+    0: {
+        'id': 0,
+        'name': 'SUPER FAMILY',
+        'admin': 0,
+    }
+}
+
+class FamilyMember(BaseModel):
+    id: int
+    name: str
+    registration: str
+
+
+class Family(BaseModel):
+    id: int
+    name: str
+    members: list[FamilyMember]
+    admin: int
+
+
+@app.get("/family", response_model=Family)
+async def get_family(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+
+    family = fake_families_db[current_user.family_id]
+    members = [{
+        'name': user['full_name'],
+        'id': user['user_id'],
+        'registration': user['registration_date']
+    } for user in fake_users_db.values() if user['family_id'] == current_user.family_id]
+    family['members'] = members
+    return family
+
+
+class NewMember(BaseModel):
+    name: str
+    email: EmailStr
+
+
+@app.post("/family")
+async def create_family_member(
+    member: NewMember,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    request: Request,
+):
+    if len(list(filter(lambda x: x['email'] == member.email, fake_users_db.values()))):
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User with this email already exist",
+        )
+    last_id = max([u['user_id'] for u in fake_users_db.values()]) + 1
+    fake_users_db[member.email] = {
+        'user_id': last_id,
+        'full_name': member.name,
+        'email': member.email,
+        'family_id': current_user.family_id, 'disabled': 1
+    }
+    token = create_access_token({'email': member.email}, timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    target_email = member.email
+    activation_link = f"{request.headers.get('referer')}activate/{token}"
+    msg = MIMEText(f"Click the link to activate your account: {activation_link}")
+    msg["Subject"] = "Activate your account"
+    msg["From"] = config['email']
+    msg["To"] = target_email
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(config['email'], config['passw'])
+        server.sendmail(config['email'], [target_email], msg.as_string())
+        return True
+
+
+class ActivationData(BaseModel):
+    password: str
+
+
+@app.post("/activate/{token}")
+def activate_user(token: str, activation_data: ActivationData):
+    print(fake_users_db)
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = dict(payload)['email']
+    except (PyJWTError, KeyError):
+        raise HTTPException(status_code=400, detail="Invalid token")
+
+    if email not in fake_users_db:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if not fake_users_db[email]['disabled']:
+        raise HTTPException(status_code=400, detail="User already activated")
+
+    fake_users_db[email]['disabled'] = 1
+    fake_users_db[email]['registration_date'] = 'today'
+    fake_users_db[email]['password'] = get_password_hash(activation_data.password)
+    return {"msg": "Account activated successfully"}
+
